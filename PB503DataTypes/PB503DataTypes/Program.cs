6c59ccecd1f20c7c7608f5eb84d6637771b57ca4
josephguluzada <yquluzade98@gmail.com>
2024-10-06 19:42:55 +0400

    added lesson project

---
+++ PB503DataTypes/PB503DataTypes/Program.cs
@@ -0,0 +1,129 @@
+﻿namespace PB503DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Arithmetic
+            //int age = 18;
+
+            //int otherAge = age + 5;
+            //Console.WriteLine(otherAge);
+
+            //int otherAgeTest = age - 8;
+
+            //Console.WriteLine(otherAge);
+
+            //var modulus = 15 / 2d;
+
+            //modulus = 444 % 1677;
+
+            //Console.WriteLine(modulus);
+
+            //int age = 17;
+
+            //age++; // age = age + 1 // 17 
+            //age--; // age = age - 1
+
+            //Console.WriteLine(age-- + --age); // 32
+
+            //Console.WriteLine(age); // 17? 16?
+
+            //Console.WriteLine(++age); // 18
+            //Console.WriteLine(--age); // 17
+            #endregion
+
+            #region Assignment 
+
+            //bool check = true;
+
+            //int number1 = 5;
+            //number1 += 5; // number1 = number1 + 5
+            //number1 -= 7; // number1 = number1 - 7
+            //number1 *= 2; // number1 = number1 * 2
+            //number1 /= 3; // number1 = number1 / 3
+            //number1 %= 2; // number1 = number1 / 3
+            //Console.WriteLine(number1);
+
+            #endregion
+
+            #region Comparison
+            //int number1 = 19;
+            //int number2 = 20;
+
+            //bool check = number1 == number2;
+            //Console.WriteLine(check);
+            //check = number1 != number2;
+            //Console.WriteLine(check);
+            //check = number1 <= number2;
+            //Console.WriteLine(!check);
+            #endregion
+
+            #region Logical
+
+            //int number1 = 70;
+            //int number2 = 42;
+
+            //bool check = (number1 == number2 || number1 > number2);
+
+            //check = (number2 <= number1 && number1 >= number2 && number2 == number2 && number2 > number1);
+
+            //Console.WriteLine(check);
+            //Console.WriteLine(!check);
+            #endregion
+
+            #region Naming Conventions
+            //int humanAgeCheckItNow98 = 18; // camelCase
+            //string HumanNameIsIt = "Salam"; // PascalCase
+            //int _counter = 0; // _private
+            //string name_human_test = "test"; // snake_case
+            ////bool check-it = false;  kabab-case
+            #endregion
+
+            #region Conditions
+
+            //var number = 72;
+
+            //if(number % 2 == 0)
+            //{
+            //    Console.WriteLine("Cutdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Tekdir");
+            //}
+
+            //byte age = 18;
+
+            //if(age >= 18)
+            //{
+            //    Console.WriteLine("Krediti aldiniz");
+            //}
+            //else if(age == 18)
+            //{
+            //    Console.WriteLine("Tebrik edirik yeni yashinizi, krediti aldiniz!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Get boyu gelersen");
+            //}
+
+            #endregion
+
+            #region For Loop
+
+            //string evens = "Cutler: ";
+            //for(int i = 1; i <=100; i++)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        evens += i;
+            //        evens += ", ";
+            //    }
+            //}
+
+            //Console.WriteLine(evens);
+            #endregion
+        }
+    }
+}
